80ef761e283ce697bb00c23dfdb2d0ae15e72c43
Frederick Javalera <conscientiadivinus@gmail.com>
2018-06-23 16:14:39 -0600

    updated multiple files

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,26 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
     return get('users');
 }
 
+export function deleteUser(id) {
+    return del(`users/${id}`);
+}
+
 function get(url) {
-    return fetch(url).then(onSucces, onError); 
+    return fetch(baseUrl + url).then(onSucces, onError); 
+}
+
+function del(url) {
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
+
+    return fetch(request).then(onSucces, onError);
 }
 
 function onSucces(response) {
